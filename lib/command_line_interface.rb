f4e07bc24a15d65bdfbc0e312ab7a8289b4692be
github username <github email address>
2019-02-12 14:31:42 +0000

    adding moves

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -15,7 +15,7 @@ class Game
 
   def get_user_name
     input = gets.chomp.capitalize
-    @trainer = Trainer.create(name: input)
+    @trainer = Trainer.find_or_create_by(name: input)
     puts "  ..."
     system "clear"
     sleep 0.5
@@ -30,6 +30,12 @@ class Game
     get_user_name
     get_pokemon_name
     ready_to_fight?
+    create_enemy
+    system "clear"
+    battle_platform
+    attack
+    battle_platform
+    binding.pry
   end
 
   def get_pokemon_name
@@ -71,18 +77,66 @@ class Game
       puts ".1"
       sleep 1.5
       puts " "
-      puts "Goooooo #{@trainer.pokemon.name}!"
+      puts "Here we go #{@trainer.pokemon.name}, let's keep our eyes peeled."
+      sleep 2
     else
       system "exit"
     end
   end
 
-  def 
-
-  def battle_platform
-    puts "E N E M Y"
-    puts ""
+  def create_enemy
+    @enemy = Pokemon.find_by(id: rand(1..Pokemon.all.length))
+    system "clear"
+    puts " "
+    puts " "
+    puts " "
+    puts " What's that!? It's a wild #{@enemy.name}!!!"
+    sleep 3
+    puts " "
+    puts " "
+    puts " "
+    puts "Prepare to fight!"
+    sleep 2
   end
 
+  def battle_platform
+    puts "E N E M Y - Wild Pokemon"
+    puts "#{@enemy.name} HP:#{@enemy.hp}"
+    puts " "
+    puts " "
+    puts " "
+    puts " "
+    puts "press a - normal attack"
+    puts " "
+    puts "press s - special attack"
+    puts " "
+    puts "press d - defense increase"
+    puts " "
+    puts " "
+    puts " "
+    puts " "
+    puts "T R A I N E R - #{@trainer.name}"
+    puts "#{@trainer.pokemon.name} HP:#{@trainer.pokemon.hp}"
+  end
+
+  def attack
+    input = gets.chomp
+      if input == "a"
+        move = @trainer.pokemon.attack + rand(10..40) - @enemy.defense
+        if move > 0
+          @enemy.hp -= move
+        end
+      end
+      if input  == "s"
+        move = @trainer.pokemon.attack + rand(1..20) - @enemy.defense
+        @trainer.pokemon.hp += rand(1..10)
+        if move > 0
+          @enemy.hp -= move
+        end
+      end
+      if input == "d"
+        trainer.pokemon.defense += rand(1..5)
+      end
+  end
 
 end
