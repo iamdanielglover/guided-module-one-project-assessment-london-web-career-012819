6f331e42033e24e769b4a9539b4eefebea8bf605
github username <github email address>
2019-02-13 13:20:07 +0000

    time for refactoring

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -9,8 +9,32 @@ class Game
   end
 
   def welcome
-    puts "Who are you?!? What's your name?"
-    sleep 2
+    puts " "
+    puts " "
+    puts " "
+    puts " "
+    puts " "
+    puts " "
+    puts "            W E L C O M E  T O "
+    sleep 3
+    system "clear"
+    puts " "
+    puts " "
+    puts " "
+    puts " "
+    puts " "
+    puts " "
+    puts "            T H E  P O K E M O N - B A T T L E - S I M U L A T O R "
+
+    sleep 3
+    system "clear"
+    puts " "
+    puts " "
+    puts " "
+    puts " "
+    puts " "
+    puts " "
+    puts "            Please enter your name: "
   end
 
   def get_user_name
@@ -34,8 +58,8 @@ class Game
     fighting_sequence
     sleep 4
 
-    puts "                         type exit"
-    gets.chomp
+    # puts "                         type exit"
+    # gets.chomp
   end
 
   def fighting_sequence
@@ -91,9 +115,14 @@ class Game
   def get_pokemon_name
     system "clear"
     sleep 0.5
-    puts "Please state which first generation pokemon"
-    puts "you'd like to be partnered with..."
     puts " "
+    puts " "
+    puts " "
+    puts " "
+    puts " "
+    puts " "
+    puts "P l e a s e   s t a t e   w h i c h   f i r s t - g e n   p o k e m o n"
+    puts "y o u ' d   l i k e   t o   b e   p a r t n e r e d   w i t h ..."
     pokemon_choice = gets.chomp.capitalize
     pokemon = Pokemon.find_by(name: pokemon_choice)
 
@@ -107,32 +136,61 @@ class Game
     system "clear"
     puts " "
     puts "  ...hmm, #{@trainer.pokemon.name}."
-    sleep 1.5
-    puts " "
-    puts "Great, #{@trainer.pokemon.name} is a marvellous choice!"
-    puts "You'll be an awesome team."
+    sleep 2
+    system "clear"
+
+    puts "You know what #{@trainer.name}?!"
+    puts "#{@trainer.pokemon.name} is a marvellous choice!"
+    puts "You two will be an awesome team!"
     sleep 5
   end
 
   def ready_to_fight?
     system "clear"
-
-    puts "Ready?"
-    puts "    y/n"
+    puts " "
+    puts " "
+    puts " "
+    puts " "
+    puts " "
+    puts " "
+    puts "R e a d y ?  h i t  y / n"
     if gets.chomp.downcase == "y"
       system "clear"
       puts " "
       puts " "
       puts " "
-      puts "...3"
-      sleep 1
-      puts "..2"
-      sleep 1
+      puts " "
+      puts " "
+      puts " "
+      puts "3"
+      sleep 1.3
+      system "clear"
+      puts " "
+      puts " "
+      puts " "
+      puts " "
+      puts " "
+      puts " "
+      puts "2"
+      sleep 1.3
+      system "clear"
+      puts " "
+      puts " "
+      puts " "
+      puts " "
+      puts " "
+      puts " "
       puts "1"
-      sleep 1
+      sleep 1.3
+      system "clear"
+      puts " "
+      puts " "
+      puts " "
+      puts " "
+      puts " "
       puts " "
       puts "Here we go #{@trainer.pokemon.name}, let's keep our eyes peeled."
-      sleep 2
+      sleep 3
     else
       system "exit"
     end
@@ -144,12 +202,19 @@ class Game
     puts " "
     puts " "
     puts " "
+    puts " "
+    puts " "
+    puts " "
     puts " What's that!? It's a wild #{@enemy.name}!!!"
     sleep 3
+    system "clear"
     puts " "
     puts " "
     puts " "
-    puts "Prepare to fight!"
+    puts " "
+    puts " "
+    puts " "
+    puts "P r e p a r e   T o   F i g h t!"
     sleep 2
   end
 
@@ -158,29 +223,31 @@ class Game
     system "clear"
     puts "E N E M Y - Wild Pokemon"
     puts "#{@enemy.name} HP:#{@enemy.hp}"
+    puts "A T T A C K - #{@enemy.attack}"
+    puts "D E F E N S E - #{@enemy.defense}"
     puts " "
     puts " "
     puts " "
+    puts "press a - n o r m a l   a t t a c k"
     puts " "
-    puts "press a - normal attack"
+    puts "press s - s p e c i a l   a t t a c k"
     puts " "
-    puts "press s - special attack"
-    puts " "
-    puts "press d - defense increase"
-    puts " "
+    puts "press d - d e f e n s e   i n c r e a s e"
     puts " "
     puts " "
     puts " "
     puts "T R A I N E R - #{@trainer.name}"
-    puts "#{@trainer.pokemon.name} HP:#{@trainer.pokemon.hp}"
+    puts "#{@trainer.pokemon.name}   HP:#{@trainer.pokemon.hp}"
+    puts "A T T A C K - #{@trainer.pokemon.attack} "
+    puts "D E F E N S E - #{@trainer.pokemon.defense} "
   end
 
   def get_attack
     input = gets.chomp
 
       if input == "a"
-        move = (@trainer.pokemon.attack/3) + rand(20..120) - @enemy.defense/2
-        retaliation = (enemy.attack/3) + rand(20..100) - @trainer.pokemon.defense/2
+        move = (@trainer.pokemon.attack/3) + rand(20..70) - @enemy.defense/2
+        retaliation = (enemy.attack/3) + rand(20..60) - @trainer.pokemon.defense/2
         if move > 0
           @enemy.hp -= move
         else
@@ -202,13 +269,13 @@ class Game
         puts " "
         puts "#{@enemy.name} hit for #{retaliation}."
         puts "#{@trainer.name}'s' #{@trainer.pokemon.name} HP: #{@trainer.pokemon.hp}"
-        sleep 4
+        sleep 2.5
       end
 
       if input  == "s"
-        move = (@trainer.pokemon.attack/3) + rand(20..100) - @enemy.defense/2
+        move = (@trainer.pokemon.attack/3) + rand(20..50) - @enemy.defense/2
         @trainer.pokemon.hp += rand(5..35)
-        retaliation = (enemy.attack/3) + rand(40..100) - @trainer.pokemon.defense/2
+        retaliation = (enemy.attack/3) + rand(20..60) - @trainer.pokemon.defense/2
         if retaliation > 0
           @trainer.pokemon.hp -= retaliation
         else
@@ -230,13 +297,13 @@ class Game
         puts " "
         puts "#{@enemy.name} hit for #{retaliation}."
         puts "#{@trainer.name}'s' #{@trainer.pokemon.name} HP: #{@trainer.pokemon.hp}"
-        sleep 4
+        sleep 2.5
       end
 
       if input == "d"
         move = 0
         trainer.pokemon.defense += rand(5..20)
-        retaliation = (enemy.attack/3) + rand(40..100) - @trainer.pokemon.defense/2
+        retaliation = (enemy.attack/3) + rand(20..50) - @trainer.pokemon.defense/1.5
         if retaliation > 0
           @trainer.pokemon.hp -= retaliation
         else
